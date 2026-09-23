0b00bb131d14c555bbb37e9b4fe6ceca8e7f18ae
jrodriguez29-JLR <jrodriguez29@setoncatholic.org>
2026-09-23 18:20:56 +0000

    V1

---
+++ Main.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    print(x+y)
+
+def subtract(x,y):
+    print(x-y)
+
+def multiply(x,y):
+    print(x*y)
+
+def division(x,y):
+    print(x/y)
+
+x = int(input('enter first number: '))
+y = int(input('enter second number: '))
+
+add(x,y)
+subtract(x,y)
+multiply(x,y)
+division(x,y)
